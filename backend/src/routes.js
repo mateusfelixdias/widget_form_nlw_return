f135406cbaaa82dfafc43296a4f4fbdd55703a83
Mateus <felixmateus618@gmail.com>
2022-05-11 10:18:50 -0300

    update backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@ const all = require('./app/adiministrative_products/all_products');
 const deleta = require('./app/adiministrative_products/deleting_product');
 const insert = require('./app/adiministrative_products/adding _product');
 const update = require('./app/adiministrative_products/update_product');
+//const pedido = require('./app/day_and_month_billing/monthlyBilling');
+const pedido = require('./app/day_and_month_billing/billingOfTheDay');
 const routes = require('express').Router();
 
 routes.delete('/delete/:nome', deleta.delete_product);
@@ -9,6 +11,8 @@ routes.delete('/delete/:nome', deleta.delete_product);
 routes.get('/all', all.all_products);
 
 routes.post('/insert', insert.adding_product);
+
+routes.post('/pedido', pedido.billingDay);
 
 routes.put('/update/:nome', update.update_product);
 
