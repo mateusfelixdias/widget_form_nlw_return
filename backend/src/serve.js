988a2f1eb4f9bf075878576a10cc5acf452bcedd
Mateus <felixmateus618@gmail.com>
2022-05-16 15:28:15 -0300

    update frontend && backend

--- backend/src/serve.js
+++ backend/src/serve.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
     console.log(`Application running on the port ${PORT}.`)
