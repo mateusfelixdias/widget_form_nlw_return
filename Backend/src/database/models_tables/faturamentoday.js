7adc20e68ab70d6ec8d812656b8f22b085b2e052
Mateus <felixmateus618@gmail.com>
2022-05-16 15:16:31 -0300

    deleted

--- Backend/src/database/models_tables/faturamentoday.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (Sequelize, DataTypes) {
-    const faturamentoday = Sequelize.define('faturamentoday', {
-        nome:  DataTypes.STRING,
-        total: DataTypes.INTEGER,
-        faturamentoday: DataTypes.FLOAT,
-    });
-
-    return faturamentoday;
-};
--- Backend/src/serve.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-    console.log(`Application running on the port ${PORT}.`)
-});
