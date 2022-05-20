696c93d89b51418fcc19a9ba853971caca271322
Mateus <felixmateus618@gmail.com>
2022-05-20 15:30:53 -0300

    update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import App from './App'
-import { BakeryAdd } from './Bakery/BakeryProdutos';
-import { BakeryLogin } from './Bakery/BakeryLogin';
+import { BakeryAdd } from './components/BakeryProdutos';
+import { BakeryLogin } from './components/BakeryLogin';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import { Routes, Route } from "react-router-dom";
@@ -12,9 +12,9 @@ const roots = ReactDOM.createRoot(document.getElementById('root')!)
 roots.render(
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="login" element={ <BakeryLogin /> } />
-      <Route path="insert" element={ <BakeryAdd /> } />
+      <Route path="/" element={ <App /> } />
+      <Route path="/login" element={ <BakeryLogin /> } />
+      <Route path="/insert" element={ <BakeryAdd /> } />
     </Routes>
   </BrowserRouter>
 )
