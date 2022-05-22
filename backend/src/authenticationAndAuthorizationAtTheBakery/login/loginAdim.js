6a3f3737ad237659e5d7b215882c7412e1b2c48f
Mateus <felixmateus618@gmail.com>
2022-05-22 08:43:20 -0300

    modified: loginAdim.

--- backend/src/authenticationAndAuthorizationAtTheBakery/login/loginAdim.js
+++ backend/src/authenticationAndAuthorizationAtTheBakery/login/loginAdim.js
@@ -1,4 +1,5 @@
 const validationData = require('../validationLogin/validationLoginAdim');
+const { gerente } = require('../../database/modelsTables');
 const token = require('../token/tokenAdim/token');
 
 
@@ -6,22 +7,41 @@ class Login {
     async loginAdim ( req, res ){
         const { email, password } = req.body;
 
+        //Validando os dados.
         const validation = await validationData( { email, password } );
 
+
         if(typeof validation === 'object'){
-            const tokenAdim = await token( { id: validation.id } ) ;
+            const tokenAdim = await token( { id: validation.id, email } );
+
 
             if(typeof tokenAdim === 'object'){
-                res.send(tokenAdim);
+                const [ , tokenAlreadyValidated ] = await req.headers.authorization.split(' ');
+
+
+                const path = {
+                    token: { token: tokenAlreadyValidated }, 
+                    multi: { multi: true },
+                    where: { where: { email: email } },
+                };
+
+                const newToken = await gerente.update(
+                    path.token,
+                    path.where,
+                    path.multi,
+                    () => {{}}
+                );
+
+                res.send(newToken[0] === 1 ? true : false );
 
             }else{
-                res.send(tokenAdim);
+                res.send( tokenAdim );
+
             };
         }else{
-            res.send(validation);
+            res.send( validation );
 
         };
     };
 };
-
 module.exports = new Login();
