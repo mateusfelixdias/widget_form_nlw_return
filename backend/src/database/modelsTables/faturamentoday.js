330408bffcf7114219563fde0ef9d2cc0c9973cb
Mateus <felixmateus618@gmail.com>
2022-06-14 10:36:39 -0300

    deleted project.

--- backend/src/database/modelsTables/faturamentoday.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (Sequelize, DataTypes) {
-    const faturamentoday = Sequelize.define('faturamentoday', {
-        name:  DataTypes.STRING,
-        vendidos: DataTypes.INTEGER,
-        faturamentoday: DataTypes.FLOAT,
-    });
-
-    return faturamentoday;
-};
--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1>Olá...</h1>
-  );
-};
-
-export default App;
