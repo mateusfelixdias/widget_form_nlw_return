3fd77d3d3c2c9676511461bd98541adeb37aaf48
Mateus <felixmateus618@gmail.com>
2022-06-16 11:48:38 -0300

    added feedback sucess step.

---
+++ frontend/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -0,0 +1,34 @@
+import { CloseButtom } from "../../CloseButtom";
+
+interface FeedbackSuccessStepProps {
+    onFeedbackRestarRequested: () => void;
+
+};
+
+export function FeedbackSuccessStep(
+    { onFeedbackRestarRequested }: 
+    FeedbackSuccessStepProps){
+    return (
+        <>
+            <header>
+                <CloseButtom />
+            </header>
+
+            <div className="flex flex-col items-center py-10 w-[304px]">
+
+                <span>
+                    Agradecemos o Feedback :)
+                </span>
+
+                <button
+                type="button"
+                    onClick={ onFeedbackRestarRequested }
+                    className="py-2 px-6 mt-6 bg-zinc-600 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus-ring-brand-500 transsition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+                >
+                    Quero enviar outro
+                </button>
+
+            </div>
+        </>
+    );
+};
