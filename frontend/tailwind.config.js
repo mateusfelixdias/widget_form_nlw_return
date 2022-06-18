f1e4eb223ecb6c7926a09121921ec122d7d0a1af
Mateus <felixmateus618@gmail.com>
2022-06-18 14:46:04 -0300

    added largura min para mobile.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
         borderRadius: {
           md: '4px'
         }
-      }
+      },
+      screens: {
+        'mobilemin': { 'min': "100px", 'max': "300px" },
+        'mobile': {'min': "300px", 'max': "800px" },
+      },
     },
   },
   plugins: [],
